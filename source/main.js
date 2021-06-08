f420ff2d7f8133dd7e2d7f3264ac9a48eb403b51
Jerick Iquin - Home <jci.dev.0@gmail.com>
2021-06-07 17:56:32 -0700

    Added HTML boiler plate, minor styling and updated JS

--- source/main.js
+++ source/main.js
@@ -1,14 +1,28 @@
 const url = 'http://www.boredapi.com/api/activity';
+const typeSelectList = ['education', 'recreational', 'social', 'diy', 'charity', 'cooking', 'relaxation', 'music', 'busywork'];
+const descriptionList = [
+    'Type of activity',
+    'Number of participants that this activity could involve',
+    'A factor describing how possible an event is to do with zero being most accessible'
+];
 
-async function getActivity() {
+//DOM
+const body = document.body;
+const typeSelect = document.getElementById('type-select');
+const description = document.getElementById('description');
+const submitQuery = document.getElementById('submit');
+const randomQuery = document.getElementById('random');
+const resultDisplay = document.getElementById('result');
+const activityForm = document.getElementById('activity-form')
+
+
+async function getActivity(queryObj) {
     try {
-        const queryObj = getParametersFromForm();
         const response = await fetch(setFullQuerry(queryObj));
         const responseJson = await response.json();
-        if (responseJson.error) {
-            throw new Error(responseJson.error);
-        }
+
         // display data here
+        renderResult(responseJson);
 
     } catch (err) {
         console.log(err);
@@ -19,10 +33,15 @@ async function getActivity() {
 // parameters from form
 function getParamatersFromForm() {
     // get data from DOM
+    const formData = new FormData(activityForm);
+    const formArray = [];
+    for (let element of formData) {
+        formArray.push(element);
+    }
     return {
-        type,
-        participants,
-        accessibility
+        type: formArray[0][1],
+        participants: formArray[1][1],
+        accessibility: formArray[2][1]
     }
 }
 
@@ -45,6 +64,63 @@ function setFullQuerry(queryObj) {
         
     }
     return url + stringToPass;
+}
+
+function renderResult(jsonResult) {
+    if (resultDisplay.firstChild) {
+        resultDisplay.removeChild(resultDisplay.firstChild);
+    }
+
+    const result = document.createElement('p');
+    var resultString = '';
+
+    if (jsonResult.error) {
+        resultString += 'No activity found with the specified parameters'
+
+    } else {
+        for (let key in jsonResult) {
+            resultString += `${key}: ${jsonResult[key]}\n`;
+        }
+
+    }
+    result.innerText = resultString;
+    resultDisplay.appendChild(result);
+}
+
+body.onload = () => {
+    descriptionList.forEach((desc, idx) => {
+        const tableRow = document.createElement('tr');
+        const col1 = document.createElement('td');
+        col1.innerText = `${idx + 1}.`;
+        col1.style.padding = '0 1em';
+        col1.style.verticalAlign = 'top';
+        const col2 = document.createElement('td');
+        col2.innerText = desc;
+        col2.style.padding = '0 1em';
+        tableRow.append(col1,col2);
+        description.appendChild(tableRow);
+    })
+    typeSelectList.forEach(activity => {
+        var option = document.createElement('option');
+        option.value = activity;
+        option.innerText = activity[0].toUpperCase() + activity.slice(1);
+        typeSelect.appendChild(option)
+    })
+}
+
+randomQuery.onclick = (e) => {
+    e.preventDefault();
+    getActivity({
+        type: '',
+        participants: '',
+        accessibility: '',
+    });
+}
+
+submitQuery.onclick = (e) => {
+    e.preventDefault();
+    const formObj = getParamatersFromForm();
+    getActivity(formObj);
 }
 
 
